cc54f9346dfa25dbc6a2207c0fa0c028604944f2
Ishita Singh <isingh05439@gmail.com>
2026-01-21 20:58:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+//const para=document.getElementById('para')
+//para.innerText="This is updated paragraph"
+//para.style.color="green"
+//console.log(para)
+
+//const para=document.getElementsByClassName('para')
+//para[0].innerText="This is updated paragraph"
+//para[1]
+
+//ele)
+
+//const container=document.querySelector(".container")
+//container.innerHTML="<h1>This is a heading</h1>"
+//console.log(container)
+
+//const start = document.querySelector('#start');
+//const stop = document.querySelector('#stop');
+
+//start.classList.add("btn");
+
+//function message(event) {
+//console.log("button clicked");
+//}
+//start.addEventListener('keyup',message)
+
+//start.addEventListener('click', message);
+
+//stop.addEventListener('click', () => {
+    //console.log("stop button clicked!!");
+    
+    //start.removeEventListener('click', message);
+//});
+
+//const form=document.querySelector('form')
+//form.addEventListener('submit',(event)=>{
+//event.preventDefault()
+   // console.log("Form Submitted")
+//})
+
+const div=document.querySelector('.container')
+const div2=document.querySelector('outer')
+const btn=document.querySelector('button')
+
+
+div.addEventListener('click',()=>{console.log("clicked the div")})
+div2.addEventListener('click',()=>{console.log("clicked the outer div")})
+btn.addEventListener('click',()=>{console.log("clicked the btton")})
