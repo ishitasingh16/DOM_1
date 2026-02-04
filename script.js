e5197f45beceaf251a834cc4b7a47b1a0332b361
Ishita Singh <isingh05439@gmail.com>
2026-02-04 15:47:10 +0530

    event

--- script.js
+++ script.js
@@ -55,10 +55,137 @@
 
 // greet();
 // console.log(message); 
-const timerId=setInterval(()=>{
-    console.log("After 3 sec")
-},1000)
-console.log(timerId)
-setTimeout(()=>{
-    classInterval(timerId)
-},10*1000)
+// const timerId=setInterval(()=>{
+//     console.log("After 3 sec")
+// },1000)
+// console.log(timerId)
+// setTimeout(()=>{
+//     classInterval(timerId)
+// },10*1000)
+// let count = 1;
+
+// cont timerId=setInterval
+// })
+// function greet(){
+//  console.log("hello students")
+// }
+// function print(sample, num){
+//     console.log("total students are ",num)
+// }
+// print(greet, 45)
+// console.log("Starting homework...");
+
+// setTimeout(() => {
+//   console.log("Homework done!");
+//   console.log("Starting dinner...");
+
+//   setTimeout(() => {
+//     console.log("Dinner done!");
+//     console.log("Getting ready to go out...");
+
+//     setTimeout(() => {
+//       console.log("Going to the playground!");
+//     }, 1000); // after dinner
+
+//   }, 1500); // dinner time
+
+// }, 2000); // homework time
+
+// function finishHomework(callback){
+//     console.log("starting homework...");
+//     setTimeout(() =>{
+//         console.log("Homework done!");
+//         callback();
+        
+//     },2000);
+// }
+// function eatDinner(callback){
+//     console.log("starting dinner...");
+//     setTimeout(() =>{
+//         console.log("dinner done!");
+//         callback();
+        
+//     },1500);
+// }
+ 
+// function goToplayground(){
+//     console.log("going to playground!");
+// }
+
+// finishHomework(()=>{
+//     eatDinner(() =>{
+//         goToplayground();
+//     });
+// });
+//  cost p=new Promise ((res,rej)=>{
+//     let done=false
+//     setTimeout(()=>{
+//         if(done){
+//             res("Work is done!!")
+//         }else{
+//             rej("work is done")
+//         }
+//     },5000)
+//  })
+// p.then(msg)=>{
+//     console.log(msg)
+
+// }.catch(err)=>
+    function doHomework() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let homeworkDone = true;
+            if (homeworkDone) {
+                console.log("Homework is done");
+                resolve("Homework complete");
+            } else {
+                reject("Homework not done");
+            }
+        }, 3000);
+    });
+}
+
+function eatDinner() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let dinnerEaten = true;
+            if (dinnerEaten) {
+                console.log("Dinner is eaten");
+                resolve("Dinner complete");
+            } else {
+                reject("Didn't eat dinner");
+            }
+        }, 2000);
+    });
+}
+
+function goToPlayground() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let permission = true;
+            if (permission) {
+                console.log("Went to the playground");
+                resolve("Playground time!");
+            } else {
+                reject("Not allowed to go out");
+            }
+        }, 3000);
+    });
+}
+
+doHomework()
+  .then((data) => {
+    console.log(data);
+    return eatDinner();
+  })
+  .then((data) => {
+    console.log(data);
+    return goToPlayground();
+  })
+  .catch((error) => {
+    console.log("Something went wrong:", error);
+  })
+  .finally(() => {
+    console.log(" Go to sleep");
+  });
+
